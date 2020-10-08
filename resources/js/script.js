0d5a07a09b5c7952099cc44ab654896d92177ba8
James Fisher <jfisher396@hotmail.com>
2020-10-08 08:53:21 -0700

    nav links connected

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,18 +1,37 @@
 console.log("script.js is working");
 
-$(document).ready(function() {
+$(document).ready(function () {
+  // sticky navbar animation
+  $(".js--section-features").waypoint(
+    function (direction) {
+      if (direction == "down") {
+        $("nav").addClass("sticky");
+      } else {
+        $("nav").removeClass("sticky");
+      }
+    },
+    {
+      offset: "60px;",
+    }
+  );
 
-    $('.js--section-features').waypoint(function(direction) {
-        if(direction == "down") {
-            $('nav').addClass('sticky');
-        } else {
-            $('nav').removeClass('sticky');
-        }
-    }, {
-        offset: '60px;'
-    })
+//   scroll to plans section animation
+  $(".js--scroll-to-plans").click(function () {
+    $("html, body").animate(
+      { scrollTop: $(".js--section-plans").offset().top },
+      1000
+    );
+  });
+
+//   scroll to features section animation
+  $(".js--scroll-to-features").click(function () {
+    $("html, body").animate(
+      { scrollTop: $(".js--section-features").offset().top },
+      500
+    );
+  });
+
+  
 
 
-
-
-})
+});
